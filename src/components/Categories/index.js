a7e0d2ee9b1eb0571c52b2e85a5f7c1fd0483998
jahar singh <jaharsingh613@gmail.com>
2023-06-14 09:14:40 +0530

    first commit

---
+++ src/components/Categories/index.js
@@ -0,0 +1,27 @@
+
+import React,{ useEffect, useState } from 'react'
+import FeatureCard from '../FeatureCard'
+
+const Categories = () => {
+    const [categories, setCategories] = useState([])
+    useEffect(() => {
+        const fetchCategories = async () => {
+            const response = await fetch('https://fakestoreapi.com/products/categories')
+            const data = await response.json()
+            console.log(data, 'data')
+            setCategories(data)
+        }
+        fetchCategories()
+    }, [])
+
+    if (categories.lenth === 0) return <div>Loading.....</div>
+
+    return (
+        
+            <FeatureCard cards={categories} />
+           
+        
+    )
+}
+
+export default Categories;
